58f0e5de9f8b044d0046b865ac4635e814cc167e
unknown <tkdgusdl63@naver.com>
2023-03-20 14:23:26 +0900

    feat: 프론트 기본세팅 #5

---
+++ front/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import tw from 'tailwind-styled-components';
+import logo from './logo.svg';
+import './App.css';
+
+const Tests = tw.p`bg-slate-600 text-red-600 text-center`;
+function App() {
+  return (
+    <div className="App">
+      <Tests>
+        유헌상
+        바보ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+      </Tests>
+    </div>
+  );
+}
+
+export default App;
