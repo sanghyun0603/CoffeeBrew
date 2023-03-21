bd162b6f9207ba1c07c9116254ffcd0259a6d2f3
jaeuk <hju9707@naver.com>
2023-03-21 13:13:10 +0900

    feat: 회원정보 수정 설정 및 Swagger 수정 #12

--- server/src/main/java/b305/coffeebrew/server/config/utils/ResponseDTO.java
+++ server/src/main/java/b305/coffeebrew/server/config/utils/ResponseDTO.java
@@ -1,6 +1,8 @@
 package b305.coffeebrew.server.config.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
@@ -10,10 +12,13 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude
+@ApiModel("ResponseDTO")
 public class ResponseDTO {
-
+	@ApiModelProperty(name="응답 코드", example = "200")
 	private HttpStatus status;
+	@ApiModelProperty(name="응답 메시지", example = "정상")
 	private String message;
+	@ApiModelProperty(name="응답값", example = "value")
 	private Object value;
 
 	public ResponseDTO(HttpStatus status, String message) {
