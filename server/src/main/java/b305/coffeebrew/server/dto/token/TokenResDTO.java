a7577af912551d3ecd95ee5fc4ce752b8f106001
이동훈 <mankind1990s@gmail.com>
2023-03-24 13:27:38 +0900

    merge: feat/be/coffeebrew-25 -> dev #25

--- server/src/main/java/b305/coffeebrew/server/dto/token/TokenResDTO.java
+++ server/src/main/java/b305/coffeebrew/server/dto/token/TokenResDTO.java
@@ -3,19 +3,18 @@ package b305.coffeebrew.server.dto.token;
 import b305.coffeebrew.server.config.utils.RedisUtil;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class TokenResDTO {
 	private int code;
 	private String token;
-
-	// RedisUtil 주입
-	private final RedisUtil redisUtil;
+	private RedisUtil redisUtil;
 
 	public String getUserEmail() {
 		String userEmail = null;
-		// RedisUtil을 이용하여 memberEmail 가져오기
 		try {
 			userEmail = redisUtil.getData(token);
 		} catch (Exception e) {
