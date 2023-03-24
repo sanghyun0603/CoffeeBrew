523684a28ed5db59f9efdd7694be97359a598ab5
dostiny <dogyeom2@gmail.com>
2023-03-24 20:01:00 +0900

    fix: full page 오류 해결

--- front/src/components/main/index.tsx
+++ front/src/components/main/index.tsx
@@ -2,3 +2,4 @@ export { default as Page1 } from './Page1';
 export { default as Page2 } from './Page2';
 export { default as Page3 } from './Page3';
 export { default as Dots } from './Dots';
+export { default as Test } from './Test';
