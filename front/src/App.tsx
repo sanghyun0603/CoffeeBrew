9dbb3431ef06141270f90f27a77c5e63605a34cb
unknown <kweony1123@naver.com>
2023-03-24 13:22:29 +0900

    fix: merge conflicts

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import tw from 'tailwind-styled-components';
 import Navbar from './components/navbarandfoot/Navbar';
 import Footer from './components/navbarandfoot/Footer';
@@ -6,29 +5,15 @@ import Router from './Router';
 import './App.css';
 
 function App() {
-  const [pageRoute, setPageRout] = useState<boolean>(false);
-  // const [nn, setNN] = useState<number>(9);
-  // console.log(pageRoute);
-  if (pageRoute === true) {
-    return (
-      <div className="App">
-        <Navbar />
-        <MainContainer>
-          <Router />
-        </MainContainer>
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <Navbar />
-        <ContentContainer>
-          <Router />
-        </ContentContainer>
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Navbar />
+      <MainContainer>
+        <Router />
+      </MainContainer>
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 const ContentContainer = tw.div`mt-10vh min-h-90vh flex flex-col`;
