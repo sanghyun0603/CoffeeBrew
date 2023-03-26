bfedbbeed62ee2c75fe425d2336c336a94b59b8a
dostiny <dogyeom2@gmail.com>
2023-03-27 00:49:21 +0900

    feat: 캐러셀 조금 수정 #37

--- front/src/components/main/Page1.tsx
+++ front/src/components/main/Page1.tsx
@@ -4,6 +4,7 @@ const Page1 = () => {
   return (
     <Inner>
       <ItemDiv>
+        {/* <DivDiv></DivDiv> */}
         <TextDiv>
           내 입맛에 맞는 <br /> 원두 추천받기
         </TextDiv>
@@ -15,7 +16,7 @@ const Page1 = () => {
 
 export default Page1;
 
-const Inner = tw.div`h-screen flex flex-row justify-end items-end bg-mainBg1 bg-cover`;
+const Inner = tw.div`h-screen flex flex-row justify-start items-end bg-mainBg1 bg-cover`;
 const ItemDiv = tw.div`flex flex-col justify-center items-center mb-1/10 mx-1/10`;
-const TextDiv = tw.div`text-black text-6xl font-bold m-4 right-1/2 -bottom-3/4 text-right`;
+const TextDiv = tw.div`text-black text-6xl font-bold m-4 right-1/2 text-left`;
 const MoveButton = tw.button`bg-transparent hover:bg-mainOrige text-6xl text-mainOrige font-semibold hover:text-white py-4 px-2 border border-mainOrige hover:border-transparent rounded`;
--- front/src/components/main/Page3Carousel.tsx
+++ front/src/components/main/Page3Carousel.tsx
@@ -1,12 +1,13 @@
 import tw from 'tailwind-styled-components';
 import Carousel from 'react-material-ui-carousel';
 import { Paper, Button } from '@mui/material';
+import bg1 from '../../assets/mainImg/bg3-1.jpg';
+import bg2 from '../../assets/mainImg/bg3-2.jpg';
+import bg3 from '../../assets/mainImg/bg3-3.jpg';
 
 interface Item {
   [key: string]: string;
 }
-
-type ItemType = Item[];
 
 interface ItemProps {
   key: number;
@@ -16,14 +17,17 @@ interface ItemProps {
 const Page3Carousel = () => {
   const items = [
     {
+      bg: 'bg1',
       title: '어떤 나라에서 커피를 생산할까요?',
       url: 'https://www.naver.com/',
     },
     {
+      bg: 'bg2',
       title: '커피 기구는 어떤 것이 있을까요?',
       url: 'https://www.naver.com/',
     },
     {
+      bg: 'bg3',
       title: '커피의 역사에 대해서 알아볼까요?',
       url: 'https://www.naver.com/',
     },
@@ -39,19 +43,21 @@ const Page3Carousel = () => {
 
 const Item = (props: ItemProps) => {
   return (
-    <Paper>
+    <BgDiv style={{ backgroundImage: `url(${bg1})` }}>
       <TextDiv>{props.item.title}</TextDiv>
-      <Button
+      <MoveButton
         onClick={() => {
           window.open(`${props.item.url}`);
         }}
       >
         SHOW NOW
-      </Button>
-    </Paper>
+      </MoveButton>
+    </BgDiv>
   );
 };
 
 export default Page3Carousel;
 
-const TextDiv = tw.div`text-black text-2xl`;
+const BgDiv = tw.div`h-screen`;
+const TextDiv = tw.div`text-black text-2xl mx-1/10`;
+const MoveButton = tw.button`text-white text-2xl mx-1/10`;
