e585a3d9f7d45b00a7b2f66708fa3a778ac41474
권동규 <ratsae0117@gmail.com>
2023-03-27 10:51:45 +0900

    feat: add api path #43

--- server-recom/app/main.py
+++ server-recom/app/main.py
@@ -3,18 +3,54 @@ import os.path as path
 import re, math
 import json
 
+from typing import Union
+
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 
-URI_PREFIX = "/api"
+URI_PREFIX = "/api/recom"
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    pass
+    return {"message": "Hello FastAPI !!"}
+
+
+# cbf 기반 원두 추천 알고리즘 호출
+@app.get(URI_PREFIX + "/bean")
+async def getBeanInfoAll():
+    pass
+    return {"message": "call /bean"}
+
+
+@app.get(URI_PREFIX + "/bean/{beanId}")
+async def getBeanRecom(beanId: Union[int, None] = None):
+    pass
+    return {"message": f"call /bean/{beanId}"}
+
+
+# cf 기반 추천 알고리즘 호출
+@app.get(URI_PREFIX + "/user")
+async def getUserInfoAll():
+    pass
+    return {"message": "call /user"}
+
+
+@app.get(URI_PREFIX + "/user/{userId}")
+async def getUserRecom(userId: Union[int, None] = None):
+    pass
+    return {"message": f"call /user/{userId}"}
+
+
+# 스케줄러에서 추천 데이터 최신화를 요청할 때 호출
+@app.get(URI_PREFIX + "/update")
+async def updateRecom():
+    pass
+    return {"message": "call /update"}
 
 
 if __name__ == "__main__":
