ecaa0b6c140208c44cd92445a0247eed3312b2e9
unknown <tkdgusdl63@naver.com>
2023-03-29 10:42:00 +0900

    feat/fe 로그인리덕스추가 #50

--- front/src/pages/Redirect.tsx
+++ front/src/pages/Redirect.tsx
@@ -1,15 +1,19 @@
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { setAccessToken, AppDispatch } from '../store';
 
 const Redirect = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+
   useEffect(() => {
     const getToken = async () => {
       try {
         const url = new URL(document.location.href).searchParams;
-        console.log(url);
         const accessToken = url.get('accessToken');
-        localStorage.setItem('accessToken', 'Bearer ' + accessToken);
+        dispatch(setAccessToken('Bearer' + accessToken));
+        // localStorage.setItem('accessToken', 'Bearer ' + accessToken);
         setTimeout(() => navigate('/', { replace: true }), 500);
       } catch (e) {
         console.log(e);
