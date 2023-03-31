42cc0dfddb88795a7e26ada85b27321b7f20d0bb
배상현 <tkdgusdl63@naver.com>
2023-03-31 21:43:30 +0900

    feat/fe 리스트트props넘겨주기 #72

--- front/src/components/ProductList/AllList.tsx
+++ front/src/components/ProductList/AllList.tsx
@@ -8,12 +8,55 @@ import Paging from './Pagings';
 import { listAPI } from '../../api/api';
 const AllListBg = tw.div`flex-col bg-background select-none`;
 
+interface BeanResponseType {
+  content: BeanType[];
+  pageable: {
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+
+export interface BeanType {
+  idx: number;
+  nameKo: string;
+  nameEn: string;
+  summary: string;
+  thumbnail: string | null;
+  userGrade: number | null;
+}
+
 const AllList = () => {
+  const [pagination, setPagination] = useState<BeanResponseType | null>(null);
+
   useEffect(() => {
     const getLists = async () => {
       await listAPI
         .getBeans('page=0')
-        .then((request) => console.log(request.data))
+        .then((request) => {
+          console.log(request.data.value);
+          setPagination(request.data.value);
+        })
         .catch((e) => console.log(e));
     };
     getLists();
@@ -22,7 +65,8 @@ const AllList = () => {
   return (
     <AllListBg>
       <SearchBar />
-      <CoffeeList />
+      {pagination ? <CoffeeList listDatas={pagination?.content} /> : null}
+
       <Paging />
     </AllListBg>
   );
