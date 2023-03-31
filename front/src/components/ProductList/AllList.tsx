6b79af80e142798478624c0bee01593333d3807f
배상현 <tkdgusdl63@naver.com>
2023-03-31 20:57:22 +0900

    feat/fe 리스트 API테스트 #72

--- front/src/components/ProductList/AllList.tsx
+++ front/src/components/ProductList/AllList.tsx
@@ -4,14 +4,26 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 import CoffeeList from './CoffeeList';
-
+import Paging from './Pagings';
+import { listAPI } from '../../api/api';
 const AllListBg = tw.div`flex-col bg-background select-none`;
 
 const AllList = () => {
+  useEffect(() => {
+    const getLists = async () => {
+      await listAPI
+        .getBeans('page=0')
+        .then((request) => console.log(request.data))
+        .catch((e) => console.log(e));
+    };
+    getLists();
+  }, []);
+
   return (
     <AllListBg>
       <SearchBar />
       <CoffeeList />
+      <Paging />
     </AllListBg>
   );
 };
