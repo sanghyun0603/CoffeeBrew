fdddbc2ee09918152af22a7a2ea9d240fd09ec37
hunsang <gjstkd123@naver.com>
2023-04-04 22:16:28 +0900

    feat: 원두추천 카드화  #134

--- front/src/components/Detail/RecommendBean.tsx
+++ front/src/components/Detail/RecommendBean.tsx
@@ -12,18 +12,18 @@ import ratingempty from '../../assets/tempImg/ratingempty.png';
 import { detailAPI } from '../../api/api';
 
 // const RecboxBean = tw.div`flex justify-center w-1000  mx-auto flex-col `;
-const Body = tw.div`flex justify-center select-none w-100vw h-100vh my-10 `;
-const Container = tw.div`flex-col `;
-const Slide = tw.div`flex align-middle`;
+// const Body = tw.div`flex justify-center select-none w-100vw h-100vh my-10 `;
+// const Container = tw.div`flex-col `;
+// const Slide = tw.div`flex align-middle`;
 
-const Img = tw.img`w-64 h-64 flex-none rounded-full justify-center mx-auto drop-shadow-xl`;
-const Btn = tw.div` cursor-pointer text-8xl my-auto mx-28 hover:scale-125`;
-const Position = tw.div`mb-6 flex justify-center`;
-const Current = tw.div`bg-gray-500 rounded-full w-3 h-3 ml-5  cursor-pointer`;
-const Dot = tw(Current)`bg-orange-400 w-10 ml-5 cursor-pointer`;
+// const Img = tw.img`w-64 h-64 flex-none rounded-full justify-center mx-auto drop-shadow-xl`;
+// const Btn = tw.div` cursor-pointer text-8xl my-auto mx-28 hover:scale-125`;
+// const Position = tw.div`mb-6 flex justify-center`;
+// const Current = tw.div`bg-gray-500 rounded-full w-3 h-3 ml-5  cursor-pointer`;
+// const Dot = tw(Current)`bg-orange-400 w-10 ml-5 cursor-pointer`;
 
-const ScoreTitle = tw.div`text-xl flex justify-end drop-shadow-2xl`;
-const Score = tw.img`w-8`;
+// const ScoreTitle = tw.div`text-xl flex justify-end drop-shadow-2xl`;
+// const Score = tw.img`w-8`;
 
 interface recomType {
   flavor: number;
@@ -49,8 +49,6 @@ interface recomType {
 }
 
 const RecommendBean = (): JSX.Element => {
-  // 캐러셀 페이지 순서 확인
-  const [page, setPage] = useState(0);
   const { beanId } = useParams() as { beanId: string };
 
   const [recommendBeanList, setRecommendBeanList] = useState<
@@ -69,196 +67,48 @@ const RecommendBean = (): JSX.Element => {
     };
     recomBean();
   }, []);
-  // 캐러셀 이미지 넣을 곳
-  const images = useRef([
-    {
-      src: [bean, bean2, grinding2, machine1, spin],
-    },
-  ]);
-
-  const [current, setCurrent] = useState(0);
-  const [style, setStyle] = useState({
-    marginLeft: `-${current}00%`,
-  });
-  const imgSize = useRef(images.current.length);
-
-  // 캐러셀 이동
-  const moveSlide = (i: number) => {
-    let nextIndex = current + i;
-
-    if (nextIndex < 0) nextIndex = imgSize.current - 1;
-    else if (nextIndex >= imgSize.current) nextIndex = 0;
-
-    // page 이동 -> 넘어가면 처음 혹은 마지막으로
-    if (0 < page + i && page + i !== images.current[0].src.length) {
-      setPage(page + i);
-    } else if (page + i < 0) {
-      setPage(images.current[0].src.length - 1);
-    } else setPage(0);
-
-    setStyle({ marginLeft: `-${nextIndex}00%` }); // 변경된 nextIndex 값으로 style 변수 업데이트
-    setCurrent(nextIndex);
-    // console.log(images.current);
-  };
-
-  useEffect(() => {
-    setStyle({ marginLeft: `-${current}00%` });
-  }, [current]);
+  const BackColor: string[] = [
+    '#FFAA01',
+    '#D4AA70',
+    '#E8D2A0',
+    '#F6842B',
+    '#D3BD94',
+  ];
 
   return (
-    // <RecboxBean>
-    <Body>
-      <Container>
-        {/* 위치표시 */}
-        <Position>
-          {images.current[0].src.map((x, i) => {
-            // console.log(images.current[0].src.length - 1, i);
-            if (page !== i) {
-              return (
-                <Current
-                  key={i}
-                  onClick={() => {
-                    setPage(i);
-                  }}
-                ></Current>
-              );
-            } else if (page === i) {
-              return <Dot key={i}></Dot>;
-            }
-          })}
-        </Position>
-        <div style={{ display: 'flex', position: 'relative' }}>
-          <Btn onClick={() => moveSlide(-1)}>&lt;</Btn>
-          {/* carousel 출력 */}
-          <Slide>
-            {recommendBeanList
-              ? recommendBeanList?.map((data, i) => {
-                  const RecommendRating = {
-                    향: data.flavor,
-                    산미: data.acidity,
-                    단맛: data.sweetness,
-                    쓴맛: data.bitterness,
-                    바디감: data.body,
-                  };
-
-                  const scoreArray = Object.entries(RecommendRating);
-                  const beanScore = () => {
-                    const scoreItem = [];
-
-                    for (let i = 0; i < scoreArray.length; i++) {
-                      const score = scoreArray[i];
-                      // score[0] = 기준, score[1] = 점수
-                      // console.log(score); //  ['향', 5]
-                      // .5인지 판별
-                      const isHalfCheck =
-                        score[1] / 2 - Math.floor(score[1] / 2) > 0;
-
-                      // 점수만큼 가득찬 이미지
-                      const scoreRatingFull = [];
-                      if (Number.isInteger(score[1] / 2)) {
-                        for (let j = 0; j < score[1] / 2; j++) {
-                          scoreRatingFull.push(
-                            <Score src={ratingfull} key={j} />,
-                          );
-                        }
-                      } else {
-                        // 점수가 정수형이 아니라면 Int(score)-1 개만큼 출력
-                        for (let k = 0; k < Math.floor(score[1] / 2); k++) {
-                          scoreRatingFull.push(
-                            <Score src={ratingfull} key={k} />,
-                          );
-                        }
-                      }
-
-                      // .5라면 반개 추가
-                      const scoreRatingHalf = isHalfCheck ? (
-                        <Score src={ratinghalf} />
-                      ) : null;
-
-                      const scoreRatingEmpty = [];
-                      if (Number.isInteger(score[1] / 2)) {
-                        for (let k = 0; k < 5 - score[1] / 2; k++) {
-                          scoreRatingEmpty.push(
-                            <Score src={ratingempty} key={k} />,
-                          );
-                        }
-                      } else {
-                        for (let k = 0; k < Math.floor(5 - score[1] / 2); k++) {
-                          scoreRatingEmpty.push(
-                            <Score src={ratingempty} key={k} />,
-                          );
-                        }
-                      }
-
-                      scoreItem.push(
-                        <ScoreTitle key={i}>
-                          <p
-                            style={{ marginRight: '16px', fontWeight: 'bold' }}
-                          >
-                            {score[0]}
-                          </p>
-                          {scoreRatingFull} {scoreRatingHalf}
-                          {scoreRatingEmpty}
-                        </ScoreTitle>,
-                      );
-                    }
-
-                    return scoreItem;
-                  };
-
-                  return (
-                    <div
-                      style={{ width: '640px', fontSize: '32px' }}
-                      onClick={() => {
-                        console.log(data);
-                      }}
-                    >
-                      <div className="flexBox" style={style}>
-                        <div
-                          key={i}
-                          style={{ display: 'flex', flexDirection: 'row' }}
-                        >
-                          <Img
-                            key={i}
-                            style={{ backgroundImage: `${bean}` }}
-                            onClick={() => {
-                              console.log(data);
-                            }}
-                          />
-                          <div style={{ marginLeft: '16px', width: '450px' }}>
-                            <div
-                              style={{
-                                wordBreak: 'break-word',
-                                fontSize: '24px',
-                              }}
-                              key={i}
-                            >
-                              {data.nameKo}
-                            </div>
-                            <div
-                              style={{
-                                textAlign: 'start',
-                                fontSize: '20px',
-                                marginLeft: '8px',
-                              }}
-                            >
-                              {data.origin}({data.region})
-                            </div>
-                            <div>{beanScore()}</div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })
-              : null}
-          </Slide>
-          <Btn onClick={() => moveSlide(1)}>&gt;</Btn>
-        </div>
-      </Container>
-    </Body>
-    // </RecboxBean>
+    <ListDiv>
+      <RecomList>
+        {recommendBeanList?.map((data, i) => {
+          return (
+            <RecomItemT1 style={{ backgroundColor: BackColor[i] }}>
+              <RecomItemImg src={bean} />
+              <RecomItemB>
+                <RecomItemName>{data.nameKo}</RecomItemName>
+                <RecomItemEngName>{data.nameEn}</RecomItemEngName>
+                <RecomInfo>
+                  <div>{data.summary}</div>
+                  <div>{data.description}</div>
+                </RecomInfo>
+              </RecomItemB>
+            </RecomItemT1>
+          );
+        })}
+      </RecomList>
+    </ListDiv>
   );
 };
 
 export default RecommendBean;
+const ListDiv = tw.div` text-center`;
+
+const RecomList = tw.div` flex  mb-4 `;
+const RecomItemT = tw.div`w-44 h-320 justify-center rounded-t-2xl my-12 drop-shadow-xl hover:scale-105 hover:drop-shadow-2xl duration-300 ml-2 mr-2 `;
+const RecomItemT1 = tw(RecomItemT)`bg-brownBorder rounded-b-2xl`;
+const RecomItemImg = tw.img`w-20 h-32  mx-auto mt-4  `;
+
+const RecomItemB = tw.div`w-44 h-44  justify-center  bg-navColor mt-10 rounded-b-2xl hover:drop-shadow-2xl overflow-y-auto`;
+const RecomItemName = tw.div`text-productTextBrown pt-4 font-bold break-words mx-4 text-xl `;
+const RecomItemEngName = tw.div`text-productTextBrown font-bold break-words mx-4 `;
+const RecomInfo = tw.div`h-24 text-nameColor overflow-y-auto mx-4`;
+const ScoreTitle = tw.div`text-xl flex justify-end drop-shadow-2xl`;
+const Score = tw.img`w-4`;
