985d41e5a00e2dc06db8074adf470aa9e98545e9
배상현 <tkdgusdl63@naver.com>
2023-04-05 08:42:45 +0900

    fix/fe survey 바디수정

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -100,7 +100,16 @@ export const mainAPI = {
 export const surveyAPI = {
   /**설문조사 결과 전송 */
   postSurvey: (surveyli: number[]) =>
-    api.post(`survey`, { surveyli: surveyli }),
+    api.post(`survey`, {
+      param1: surveyli[0],
+      param2: surveyli[1],
+      param3: surveyli[2],
+      param4: surveyli[3],
+      param5: surveyli[4],
+      param6: surveyli[5],
+      param7: surveyli[6],
+      param8: surveyli[7],
+    }),
 };
 
 export const memberAPI = {
