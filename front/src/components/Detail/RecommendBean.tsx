ed709982d752eb4446443cfe833e284f368b12bf
배상현 <tkdgusdl63@naver.com>
2023-04-04 13:20:29 +0900

    fix/fe 함수호출 수정 #119

--- front/src/components/Detail/RecommendBean.tsx
+++ front/src/components/Detail/RecommendBean.tsx
@@ -42,6 +42,7 @@ const RecommendBean = (): JSX.Element => {
         })
         .catch((e) => console.log(e));
     };
+    recomBean();
   });
   console.log(recommendBeanList);
   // 캐러셀 이미지 넣을 곳
