73f1e0d3338891fcba35dcc3e526d2250437cdcb
이동훈 <mankind1990s@gmail.com>
2023-04-04 13:16:12 +0900

    feat: be/ survey mvc 기능 추가 #120

---
+++ server/src/main/java/b305/coffeebrew/server/repository/SurveyRepository.java
@@ -0,0 +1,7 @@
+package b305.coffeebrew.server.repository;
+
+import b305.coffeebrew.server.entity.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+}
