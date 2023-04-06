b1dab777ad51a13947b54f4172e12cb844213214
hunsang <gjstkd123@naver.com>
2023-04-06 15:59:48 +0900

    fix:fe/ NoAnalyze css 수정  -170

--- front/src/components/Mypage/NoAnalyze.tsx
+++ front/src/components/Mypage/NoAnalyze.tsx
@@ -4,35 +4,37 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { useNavigate } from 'react-router-dom';
 
-const AnalyzeBody = tw.div`border-2 rounded-b-lg text-center mb-4`;
-const Comment = tw.div`text-xl font-bold text-left ml-5 mb-5`;
-const UserTypeImg = tw.div`w-720 mx-auto`;
-const LinkBtn = tw.div` class="w-400 h-16 font-bold text-2xl text-white bg-brownBorder rounded-3xl cursor-pointer hover:scale-110 mx-auto my-10"`;
+const NoAnalyzeBody = tw.div`border-2 rounded-b-lg text-center mb-4`;
+const NoComment = tw.div`text-xl font-bold text-left ml-5 mb-5`;
+const NoUserTypeImg = tw.div`w-720 mx-auto`;
+const NoLinkBtn = tw.div` class="w-400 h-16 font-bold text-2xl text-white bg-brownBorder rounded-3xl cursor-pointer hover:scale-110 mx-auto my-10"`;
 
 const NoAnalyze = () => {
   const reduxData = useSelector((state: RootState) => state);
   const Navigate = useNavigate();
 
   return (
-    <AnalyzeBody
+    <NoAnalyzeBody
       style={{
         border: 'solid 4px #FD0F0F',
         minHeight: '630px',
       }}
     >
-      <UserTypeImg>
+      <NoUserTypeImg>
         <img src={noanalyze} alt="noData" style={{ marginLeft: '110px' }} />
-      </UserTypeImg>
+      </NoUserTypeImg>
 
-      <Comment>{reduxData.memberInfo?.nickname}님을 분석할 수가 없어요</Comment>
-      <LinkBtn
+      <NoComment>
+        {reduxData.memberInfo?.nickname}님을 분석할 수가 없어요
+      </NoComment>
+      <NoLinkBtn
         onClick={() => {
           Navigate('/survey');
         }}
       >
         <p style={{ marginTop: '16px' }}>설문하러가기</p>
-      </LinkBtn>
-    </AnalyzeBody>
+      </NoLinkBtn>
+    </NoAnalyzeBody>
   );
 };
 
